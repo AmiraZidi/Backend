2d7b26d30b2d22d3c031d79743b4d7b172ee79a6
AMIRA ZIDI <zidiamira290@gmail.com>
2024-12-26 11:32:22 +0100

    first commit

---
+++ connect_db.js
@@ -0,0 +1,14 @@
+const mongoose=require("mongoose");
+
+const db_connect=async()=>{
+    try {
+        let result=await mongoose.connect(process.env.DB_URI);
+        console.log("DB is connected");
+    } catch (error) {
+        console.log(error);
+    }
+
+}
+
+
+module.exports=db_connect
